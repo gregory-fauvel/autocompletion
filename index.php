c6f192c6cf0776e32d696e87eb497d15f4f3b9e8
fauvel gregory <gregory.fauvel@laplateforme.io>
2020-04-29 18:14:35 +0200

    css index.php

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
         <link rel="stylesheet" type="text/css" href="CSS/autocompletion.css">
          <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     </head>
-    <body>
-    	     <input name="search"type="text" placeholder="Search Query..." id="searchBox">
+    <body id="index">
+        <h1 id="title">Le moteur de recherche du cinema</h1>
+    	<input name="search"type="text" placeholder="Rechercher film..." id="searchBox">
    
         <div id="response"></div>
 
-
-        
         <script type="text/javascript">
             <?php
            include("INCLUDES/script.js");
